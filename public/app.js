061ad42c9cb6fad59c6e8fde0f056029e82fcd34
Dave Burt <dave.burt@allori.com.au>
2017-12-08 13:48:07 +1100

    Don't try to speak Greek without a Greek speech synthesis voice available.

--- public/app.js
+++ public/app.js
@@ -1,14 +1,31 @@
 function say() {
-  if ($('#say').length == 0){
+  if ($('#say').length == 0 || !greekVoiceAvailable()){
     return;
   }
-  var utterance = new SpeechSynthesisUtterance($('#say').innerText);
+
+  var utterance = new SpeechSynthesisUtterance($('#say .text').text());
   utterance.lang = 'el';
   speechSynthesis.cancel();
   speechSynthesis.speak(utterance);
 }
 
+function greekVoiceAvailable() {
+  var voices = speechSynthesis.getVoices();
+  for (i = 0; i < voices.length ; i++) {
+    if (voices[i].lang.substr(0, 2) == 'el') {
+      return true;
+    }
+  }
+  return false
+}
+
 $(function() {
-  say()
   $('#say button').click(function(ev) { say(); });  
+  if (greekVoiceAvailable()) {
+    say()
+  }
+  else {
+    $('#say').append('No Greek voice found. Install a Greek voice in your operating system\'s speech synthesis (TTS) to enable voice.');
+    $('#say button').attr('disabled', true);
+  }
 })
